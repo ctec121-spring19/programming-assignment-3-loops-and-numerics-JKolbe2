588210d34e42d75265590956d4073187bae1902d
Jeremy Kolbe <j.kolbe@students.clark.edu>
2019-04-29 17:31:49 -0700

    final updates

--- Prob-5/Prob-5.py
+++ Prob-5/Prob-5.py
@@ -5,19 +5,36 @@
 # YOUR NAME
 
 def main():
-    print("\nJeremy's Output")
+    print()
     print()
     
 #prices
-    pizza = 2.0
-    coke = 1.50
-    donut = 0.56
-print("pizza")
-print("coke")
-print("donut")
+pizza = 2.0
+coke = 1.50
+donut = 0.56
 
+# Item List
+print("x2 Pizza:\t\t", "$", pizza * 2)
+print("x1 Coke:\t\t", "$", coke)
+print("x2 Donut:\t\t", "$", donut * 2)
+print("---------------------------------")
+# Totals
+subtotal = pizza * 2 + coke + donut * 2
+print("Subtotal:\t\t", "$", subtotal)
 
+tax = subtotal * 0.084
+print("Tax:\t\t\t", "$", round(tax, 2))
+print()
 
+total = subtotal + tax
+print("Total:\t\t\t", "$", round(total, 2))
+print()
+# Payment
+tender = eval(input("Please enter an amount:  $ "))
+print("Tendered:\t\t", "$", tender + 0.00)
 
+change = tender - total
+print("Change:\t\t\t", "$", round(change, 2))
+print("---------------------------------")
 
 main()
